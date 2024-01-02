1990edb8a5f114bbffe039f66154908e36f8c43f
Ujjwal Jha <ujjwaljha1225@gmail.com>
2024-01-03 00:39:42 +0545

    Create an API to make request to backend to submit complete profile

--- frontend/src/api/login.js
+++ frontend/src/api/login.js
@@ -1,17 +1,19 @@
 import axios from "axios";
 
-import _ from "lodash"
+import _ from "lodash";
 
 import { setCookieValue } from "../services/cookie";
 
 export const login = async (login_credential) => {
+  console.log(login_credential);
+  const { data } = await axios.post(
+    "http://localhost:8000/api/login/",
+    login_credential
+  );
 
-    console.log(login_credential)
-    const { data } = await axios.post("http://localhost:8000/api/login/", login_credential);
-
-
-    setCookieValue("token", _.get(data, 'token.access'))
-    setCookieValue("name", _.get(data, 'info.name'))
-    setCookieValue("email", _.get(data, 'info.email'))
-    setCookieValue("user_type", _.get(data, 'info.user_type'))
-}
+  setCookieValue("token", _.get(data, "token.access"));
+  setCookieValue("name", _.get(data, "info.name"));
+  setCookieValue("email", _.get(data, "info.email"));
+  setCookieValue("user_type", _.get(data, "info.user_type"));
+  setCookieValue("id", _.get(data, "info.id"));
+};
