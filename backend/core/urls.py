e9dc030a67c869105a3db6adb2be0cc489afa0dd
Sandesh-Pyakurel <pyakurelsandesh14@gmail.com>
2024-01-03 08:50:30 +0545

    Add khalti payment gateway

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import UserRegistrationView, UserLoginView, StartupProfileView, VentureCapitalProfileView
+from .views import UserRegistrationView, UserLoginView, StartupProfileView, VentureCapitalProfileView, KhaltiPaymentView
 
 urlpatterns = [
     path('user/', UserRegistrationView.as_view()),
     path('login/', UserLoginView.as_view()),
     path('user/startup/', StartupProfileView.as_view()),
     path('user/vc/', VentureCapitalProfileView.as_view()),
+    path('khalti/', KhaltiPaymentView.as_view()),
 ]
